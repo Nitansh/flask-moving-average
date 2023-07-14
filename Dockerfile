80f8cd5e15746e5dbd2c51101d5fad33827b70dd
Nitansh Bareja <Nitansh.Bareja@thalesgroup.com>
2023-07-14 07:28:58 +0530

    Initial code for flask app which calculates RSI and Moving average
    Signed-off-by: Nitansh Bareja <Nitansh.Bareja@thalesgroup.com>

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.8.3
+RUN apt-get install ca-certificates
+
+# switch working directory
+WORKDIR /app
+
+# copy the requirements file into the image
+COPY ./requirements.txt /app/requirements.txt
+# install the dependencies and packages in the requirements file
+RUN pip3 install --trusted-host=pypi.org --trusted-host=files.pythonhosted.org --user pip-system-certs -r requirements.txt
+
+# copy every content from the local file to the image
+COPY . /app
+
+EXPOSE 5000
+ENV PORT 5000
+
+HEALTHCHECK CMD curl --fail http://localhost:5000/healthcheck || exit 1 
+
+# configure the container to run in an executed manner
+ENTRYPOINT [ "python3" ]
+
+CMD ["app.py" ]
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from datetime import date
+from jugaad_data.nse import stock_csv, stock_df
+from finta import TA
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route('/healthcheck')
+def get_healt_check():
+    df = stock_df(symbol='RELIANCE', from_date=date(2022,7,12), to_date=date(2023,7,12), series="EQ")
+    return ''
+
+
+@app.route('/')
+def get_dma():
+    response = {}
+    stock = request.args.get('symbol')
+    dma_list = request.args.get('dma').split(',')
+    df = stock_df(symbol=stock, from_date=date(2022,7,12), to_date=date(2023,7,12), series="EQ")
+    df = df.iloc[::-1]
+    rsi = TA.RSI(df)
+    response['symbol'] = stock
+    response['price'] = df.iloc[-1]['CLOSE']
+    response['rsi'] = rsi.iloc[-1]
+
+    for item in dma_list:
+        response[item] = TA.DEMA(df, int(item.split('_')[1] ) ).iloc[-1]
+
+    return str( response['rsi'] > 30 and response['rsi'] < 70 and response['price'] > response['DMA_20'] and response['price'] > response['DMA_50'] and response['price'] > response['DMA_100'] and response['price'] > response['DMA_200'])
+    
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)
