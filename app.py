589f3c21bff8cf2fa5d0ccb24a0ec5faace4b438
Nitansh Bareja <Nitansh.Bareja@thalesgroup.com>
2024-02-08 06:12:55 +0530

    Update 2.0
    Signed-off-by: Nitansh Bareja <Nitansh.Bareja@thalesgroup.com>

--- app.py
+++ app.py
@@ -3,22 +3,39 @@ from datetime import date, timedelta, datetime
 from jugaad_data.nse import stock_df, NSELive
 from finta import TA
 from waitress import serve
+import pandas as pd
+import sys
 
 from mcap import MCAP, COMPANY_NAME
 
 app = Flask(__name__)
 n = NSELive()
+pd.options.mode.copy_on_write = True
 
 PRICE_DIFF_PERCENTAGE = 1
 PRICE_DIFF_BEARISH_PERCENTAGE = 5
 MCAP_THRESHOLD = 10
 TIME_DELTA = -1
 
+
+def get_live_symbol_df( df ):
+    try:
+        stockData = n.stock_quote(df['SYMBOL'])
+        df['DATE'] = df['DATE']+ timedelta(days=1)
+        df['OPEN'] = stockData['priceInfo']['open']
+        df['PREV. CLOSE'] = df['CLOSE']
+        df['LTP'] = stockData['priceInfo']['lastPrice']
+        df['CLOSE'] = stockData['priceInfo']['lastPrice']
+        df['VWAP'] = stockData['priceInfo']['vwap']
+    except:
+        pass
+    return df
+
 @app.route('/healthcheck')
 def get_healt_check():
     print("in Health Check")
     df = stock_df(symbol='RELIANCE', from_date=date(2022,7,12), to_date=date(2023,7,12), series="EQ")
-    return ''
+    return 'working'
 
 @app.route('/live')
 def get_live_stock():
@@ -36,27 +53,30 @@ def get_live_stock():
 @app.route('/')
 def get_dma():
     response = {}
-    stock = request.args.get('symbol')
-    dma_list = request.args.get('dma').split(',')
-    one_day_before = datetime.now() + timedelta(days=-1)
-    year = one_day_before.year
-    month = one_day_before.month
-    day = one_day_before.day
-    df = stock_df(symbol=stock, from_date=date(year-1,month,day), to_date=date(year,month,day), series="EQ")
-    df = df.iloc[::-1]
-    rsi = TA.RSI(df)
-    
-    response['symbol'] = stock
-    response['id'] = stock
-    response['price'] = df.iloc[-1]['CLOSE']
-    response['rsi'] = rsi.iloc[-1]
-    response['mcap'] = MCAP.get(stock, 0)
-    response['name'] = COMPANY_NAME.get(stock, stock)
-    response['url'] = 'https://www.screener.in/company/'+ stock +'/consolidated/'
-    for item in dma_list:
-        response[item] = TA.DEMA(df, int(item.split('_')[1] ) ).iloc[-1]
-    if response['rsi'] > 20 and response['rsi'] < 70 and response['price'] > response['DMA_20'] and response['price'] > response['DMA_50'] and response['price'] > response['DMA_100'] and response['price'] > response['DMA_200']:
-        response['isBullish'] = 'true'
+    try:
+        stock = request.args.get('symbol')
+        dma_list = request.args.get('dma').split(',')
+        one_day_before = datetime.now() + timedelta(days=-1)
+        year = one_day_before.year
+        month = one_day_before.month
+        day = one_day_before.day
+        df = stock_df(symbol=stock, from_date=date(year-1,month,day), to_date=date(year,month,day), series="EQ")
+        df = df.iloc[::-1]
+        df = df._append( get_live_symbol_df(df.iloc[0]))
+        rsi = TA.RSI(df)
+        response['symbol'] = stock
+        response['id'] = stock
+        response['price'] = df.iloc[-1]['CLOSE']
+        response['rsi'] = rsi.iloc[-1]
+        response['mcap'] = MCAP.get(stock, 0)
+        response['name'] = COMPANY_NAME.get(stock, stock)
+        response['url'] = 'https://www.screener.in/company/'+ stock +'/consolidated/'
+        for item in dma_list:
+            response[item] = TA.DEMA(df, int(item.split('_')[1] ) ).iloc[-1]
+        if response['rsi'] > 20 and response['rsi'] < 70 and response['price'] > response['DMA_20'] and response['price'] > response['DMA_50'] and response['price'] > response['DMA_100'] and response['price'] > response['DMA_200']:
+            response['isBullish'] = 'true'
+    except:
+        print( "Error occurred in "+stock)
 
     return jsonify( response )
 
@@ -75,6 +95,7 @@ def get_dma_price_diff_bullish():
     day = one_day_before.day
     df = stock_df(symbol=stock, from_date=date(year-1,month,day), to_date=date(year,month,day), series="EQ")
     df = df.iloc[::-1]
+    df = df._append( get_live_symbol_df(df.iloc[0]))
     rsi = TA.RSI(df)
 
     response['symbol'] = stock
@@ -95,4 +116,5 @@ def get_dma_price_diff_bullish():
     return jsonify( response )
     
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+    port = sys.argv[1]
+    serve(app, host='0.0.0.0', port=port)
